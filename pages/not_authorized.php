fe36beb25ce63d89a8bc5e0c1cca3df816dfd12a
hagenow <hagenow.hendrik@gmail.com>
2014-05-05 16:18:07 +0200

    autogit Mo 5. Mai 16:18:07 CEST 2014

---
+++ pages/not_authorized.php
@@ -0,0 +1,4 @@
+<div class="alert alert-warning alert-dismissable">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+  <strong>Authetication Error!</strong> You're not authorized to view this content!
+</div>
